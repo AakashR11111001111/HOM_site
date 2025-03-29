0bceff2b435c24c2da096ee28323056db644a546
AakashR11111001111 <aakash04721202019@msi-ggsip.org>
2025-03-29 13:07:39 +0530

    fixed hero

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { darkModeCtx } from "../../App";
+import gsap from "gsap";
 
 const Header = ({ headerRef }) => {
     const { darkMode, setDarkMode } = useContext(darkModeCtx);
     const [isActive, setIsActive] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
+    const dropDownRef = useRef(null);
 
     const toggleDarkMode = () => {
         setDarkMode((prev) => !prev);
@@ -17,37 +19,52 @@ const Header = ({ headerRef }) => {
         setMenuOpen((val) => !val);
     };
 
+    useEffect(() => {
+        if (menuOpen) {
+           
+                gsap.from(
+                    dropDownRef.current.querySelectorAll(".ddMenu"),{ 
+                        opacity: 0,
+                        y: -10,
+                        stagger: 0.2,
+                    },
+                
+                );
+    
+        }
+    }, [menuOpen]);
+
     return (
         <>
-        
-        <nav style={{ background: darkMode ? "var(--black)" : "var(--white)" }} className={styles.nav}>
-            <ul ref={headerRef} className={styles.desktopMenu}>
-                <li><NavLink className={({ isActive }) => isActive ? styles.isActive : ""} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/"}>Home</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? styles.isActive : ""} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/about"}>About</NavLink></li>
-                <li><NavLink className={({ isActive }) => isActive ? styles.isActive : ""} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/buy-now"}>Buy Now</NavLink></li>
-                <li><img style={{ padding: "5px", cursor: "pointer" }} onClick={toggleDarkMode} src={darkMode ? "/lightMode.png" : "/darkMode.png"} alt="Dark Mode Toggle"/></li>
-            </ul>
+            <nav style={{ background: darkMode ? "var(--black)" : "var(--white)" }} className={styles.nav}>
+                <ul ref={headerRef} className={styles.desktopMenu}>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.isActive : ""} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/"}>Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.isActive : ""} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/about"}>About</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.isActive : ""} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/buy-now"}>Buy Now</NavLink></li>
+                    <li>
+                        <img style={{ padding: "5px", cursor: "pointer" }} onClick={toggleDarkMode} src={darkMode ? "/lightMode.png" : "/darkMode.png"} alt="Dark Mode Toggle" />
+                    </li>
+                </ul>
+            </nav>
 
-        </nav>
-        <nav style={{ background: darkMode ? "var(--black)" : "var(--white)" }} className={styles.nav2}>
-            <div onClick={onHamClick} className={`${styles.hamburger} ${isActive ? styles.isActive : ""}`} style={{ background: darkMode ? "var(--black)" : "var(--white)" }}>
-                <span style={{ background: darkMode ? "var(--white)" : "var(--black)" }} className={styles.hamburgerLine}></span>
-                <span style={{ background: darkMode ? "var(--white)" : "var(--black)" }} className={styles.hamburgerLine}></span>
-                <span style={{ background: darkMode ? "var(--white)" : "var(--black)" }} className={styles.hamburgerLine}></span>
-            </div>
+            <nav style={{ background: darkMode ? "var(--black)" : "var(--white)" }} className={styles.nav2}>
+                <div onClick={onHamClick} className={`${styles.hamburger} ${isActive ? styles.isActive : ""}`} style={{ background: darkMode ? "var(--black)" : "var(--white)" }}>
+                    <span style={{ background: darkMode ? "var(--white)" : "var(--black)" }} className={styles.hamburgerLine}></span>
+                    <span style={{ background: darkMode ? "var(--white)" : "var(--black)" }} className={styles.hamburgerLine}></span>
+                    <span style={{ background: darkMode ? "var(--white)" : "var(--black)" }} className={styles.hamburgerLine}></span>
+                </div>
 
-            {
-                menuOpen 
-                &&
-                <div className={`${styles.dropdown} ${menuOpen ? styles.dropdownOpen : ""}`} style={{ background: darkMode ? "var(--black)" : "var(--white)" }}>
-                    <ul>
-                        <li><NavLink onClick={onHamClick} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/"}>Home</NavLink></li>
-                        <li><NavLink onClick={onHamClick} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/about"}>About</NavLink></li>
-                        <li><NavLink onClick={onHamClick} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/buy-now"}>Buy Now</NavLink></li>
-                    </ul>
-                </div>  
-            }
-        </nav>
+                {
+                    menuOpen &&
+                    <div ref={dropDownRef} className={`${styles.dropdown} ${menuOpen ? styles.dropdownOpen : ""}`} >
+                        <ul>
+                            <li><NavLink className="ddMenu" onClick={onHamClick} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/"}>Home</NavLink></li>
+                            <li><NavLink className="ddMenu" onClick={onHamClick} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/about"}>About</NavLink></li>
+                            <li><NavLink className="ddMenu" onClick={onHamClick} style={{ color: darkMode ? "var(--white)" : "var(--black)" }} to={"/buy-now"}>Buy Now</NavLink></li>
+                        </ul>
+                    </div>
+                }
+            </nav>
         </>
     );
 };
